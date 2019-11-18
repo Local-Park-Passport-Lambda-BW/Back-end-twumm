701161966ae935d7e4767b2ab327a4d8933916ba
Martin Twum Mensah <martint.mensah@gmail.com>
2019-11-18 12:21:33 +0000

    feature: Add get method for hitting initial LPP server page Display welcome text

--- api/server.js
+++ api/server.js
@@ -8,4 +8,16 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
+server.get('/', async (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .json({
+        message: 'Welcome to the LPP - Local Park Passport!'
+      });
+  } catch (error) {
+    next(new Error('Local Park Passport is not available at the moment'));
+  }
+});
+
 module.exports = server;
