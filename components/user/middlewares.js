1270a6f719d3fede6c0cda7aea10979afd3830e2
Martin Twum Mensah <martint.mensah@gmail.com>
2019-11-19 08:56:34 +0000

    feature: Add feature to validate user object in req.body

--- components/user/middlewares.js
+++ components/user/middlewares.js
@@ -68,3 +68,17 @@ const restricted = (req, res, next) => {
     });
   }
 };
+
+const validateUserData = (req, res, next) => {
+  if (req.body.constructor === Object && Object.keys(req.body).length <= 0) {
+    res
+      .status(400)
+      .json({ message: 'User data is missing' });
+  } else if (!req.body.email || !req.body.password) {
+    res
+      .status(400)
+      .json({ message: 'Missing required *username* and *password* fields' });
+  } else {
+    next();
+  }
+};
