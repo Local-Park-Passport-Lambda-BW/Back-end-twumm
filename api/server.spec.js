b7d19fa97b396952eb2bad97ee9f3585145815a1
Martin Twum Mensah <martint.mensah@gmail.com>
2019-11-21 10:10:49 +0000

    test: Add test for main get route

---
+++ api/server.spec.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('GET /', () => {
+  it('has process.env.NODE_ENV as "testing"', () => {
+    expect(process.env.NODE_ENV).toBe('testing');
+  });
+
+  it('returns 200 OK', () => {
+    return request(server).get('/')
+      .expect(200);
+  });
+
+  it('returns welcome text', () => {
+    return request(server).get('/')
+      .then(res => {
+        expect(res.body.message).toBe('Welcome to the LPP - Local Park Passport!');
+      });
+  });
+});
